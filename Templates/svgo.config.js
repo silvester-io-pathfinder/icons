c9d32165cbfcc8116ba502a3d2c23278ffaf2418
Teun Kooijman <teun_@DESKTOP-81EBD61>
2021-10-06 23:17:55 +0200

    Second attempt at currentColor plugin.

--- Templates/svgo.config.js
+++ Templates/svgo.config.js
@@ -4,7 +4,7 @@ module.exports = {
          name: "removeDimensions"
       },
       {
-         name: "currentColors",
+         name: "convertColors",
          params: { currentColor: true }
       }
    ]
